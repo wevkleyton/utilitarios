37cd1c1a4fc5efbdaccfa092d2ede0801c34ce3a
wevkleyton <kleytoncpd@gmail.com>
2023-03-23 13:08:18 -0300

    Criando classe de Arquivo Dump

---
+++ src/main/java/com/br/utilitarios/utilitarios/bean/ArquivosDump.java
@@ -0,0 +1,55 @@
+package com.br.utilitarios.utilitarios.bean;
+
+public class ArquivosDump {
+
+	private String nome;
+	private String data;
+	private String hora;
+	private Integer id;
+
+	public ArquivosDump() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArquivosDump(String nome, String data, String hora, Integer id) {
+		super();
+		this.nome = nome;
+		this.data = data;
+		this.hora = hora;
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}
