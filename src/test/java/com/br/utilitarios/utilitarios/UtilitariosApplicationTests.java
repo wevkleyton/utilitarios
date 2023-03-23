c12030b48c1439b9412c153e74b9e246634f789b
wevkleyton <kleytoncpd@gmail.com>
2023-03-23 12:43:59 -0300

    Modificações de resoluções de problema de inicialização do projeto

---
+++ src/test/java/com/br/utilitarios/utilitarios/UtilitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.utilitarios.utilitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilitariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
