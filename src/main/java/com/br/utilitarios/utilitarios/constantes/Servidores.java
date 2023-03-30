05724faca18c92c402cc507a2254f1c2a38f8cfc
wev <kleyton.cpd@gmail.com>
2023-03-30 18:10:07 -0300

    Criacao da classe Testeconexao e a classe de constantes Servidores

---
+++ src/main/java/com/br/utilitarios/utilitarios/constantes/Servidores.java
@@ -0,0 +1,10 @@
+package com.br.utilitarios.utilitarios.constantes;
+
+public class Servidores {
+
+	public static final String BME = "172.16.40.1";
+	public static final String DBASERVER = "172.17.7.240";
+	public static final String OMV135 = "172.17.20.135";
+	public static final String TESTE_ERROR = "172.10.10.";
+	
+}
---
+++ src/main/java/com/br/utilitarios/utilitarios/services/TestaConexao.java
@@ -0,0 +1,23 @@
+package com.br.utilitarios.utilitarios.services;
+
+import java.net.InetAddress;
+
+import com.br.utilitarios.utilitarios.constantes.Servidores;
+
+public class TestaConexao {
+
+	public TestaConexao(String host) {
+		try {
+			InetAddress address = InetAddress.getByName(host);
+			boolean reacheble = address.isReachable(10000);
+			System.out.println(reacheble);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		TestaConexao teste = new TestaConexao(Servidores.TESTE_ERROR);
+		
+	}
+}
