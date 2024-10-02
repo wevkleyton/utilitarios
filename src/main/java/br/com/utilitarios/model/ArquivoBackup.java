565f6027a0a49c1208a389650c67ab10689d7818
wevkleyton <kleyton.cpd@gmail.com>
2024-10-02 16:05:37 -0300

    Ajuste no ArquivoBackup para serializacao

--- src/main/java/br/com/utilitarios/model/ArquivoBackup.java
+++ src/main/java/br/com/utilitarios/model/ArquivoBackup.java
@@ -2,7 +2,13 @@ package br.com.utilitarios.model;
 
 import java.io.Serializable;
 
+import org.hibernate.annotations.GeneratorType;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -10,8 +16,13 @@ import jakarta.persistence.Table;
 public class ArquivoBackup implements Serializable{
 
 	
-	private static final long serialVersionUID = 1L; 
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="id_arquivo", nullable = false)
 	private Integer idArq;
+	@Column(name="dt_arquivo", nullable = false)
 	private String dataArq;
 
 	public Integer getIdArq() {
